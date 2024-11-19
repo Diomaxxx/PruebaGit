d2f52fcaed64ba339b67cbbb1e2a7e5e35ccc3a4
Diomaxxx <diogofrancounivalle@gmail.com>
2024-11-19 09:31:21 -0400

    Add project files.

---
+++ PruebaGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
